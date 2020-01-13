311d23bf26e5d5cfef1e5d4ed562baa057c91a38
Nikske <wolfvdd@gmail.com>
2020-01-13 09:36:44 +0100

    Descriptions are working, technically

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,7 @@ document.getElementById("run").addEventListener("click", function() {
                 iconList.push(data['list'][i]['weather'][0]['icon']);
             }
 
-            // TEMPERATURES
+            /* TEMPERATURES */
             // Cutting all temperatures up in the 5 days
             let dayOne = tempList.slice(0,8);
             let dayTwo = tempList.slice(8,16);
@@ -44,12 +44,26 @@ document.getElementById("run").addEventListener("click", function() {
             document.getElementById("temp4").innerHTML= tempDayFour + " " + "&#8451";
             document.getElementById("temp5").innerHTML= tempDayFive + " " + "&#8451";
 
-            // DESCRIPTIONS
+            /* DESCRIPTIONS */
             // Cutting all descriptions up in the 5 days
             let descriptionDayOne = descriptionList.slice(0,8);
             let descriptionDayTwo = descriptionList.slice(8,16);
             let descriptionDayThree = descriptionList.slice(16,24);
-            
+            let descriptionDayFour = descriptionList.slice(24,32);
+            let descriptionDayFive = descriptionList.slice(32,40);
+            console.log(descriptionDayTwo);
+
+            // Inserting descriptions - Day & night
+            document.getElementById("descriptionDay1").innerHTML= "Day: " + descriptionDayOne[3];
+            document.getElementById("descriptionNight1").innerHTML= "Night: " + descriptionDayOne[6];
+            document.getElementById("descriptionDay2").innerHTML= "Day: " + descriptionDayTwo[3];
+            document.getElementById("descriptionNight2").innerHTML= "Night: " + descriptionDayTwo[6];
+            document.getElementById("descriptionDay3").innerHTML= "Day: " + descriptionDayThree[3];
+            document.getElementById("descriptionNight3").innerHTML= "Night: " + descriptionDayThree[6];
+            document.getElementById("descriptionDay4").innerHTML= "Day: " + descriptionDayFour[3];
+            document.getElementById("descriptionNight4").innerHTML= "Night: " + descriptionDayFour[6];
+            document.getElementById("descriptionDay5").innerHTML= "Day: " + descriptionDayFive[3];
+            document.getElementById("descriptionNight5").innerHTML= "Night: " + descriptionDayFive[6];
 
 
         })
